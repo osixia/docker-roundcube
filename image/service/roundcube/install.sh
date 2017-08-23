7080ee31f4e03574b335a09b9159e2649c5335e8
Bertrand Gouny <bertrand.gouny@osixia.net>
2017-08-23 16:47:59 +0200

    optimise apache and opcache config

--- image/service/roundcube/install.sh
+++ image/service/roundcube/install.sh
@@ -4,9 +4,15 @@
 cat /container/service/roundcube/assets/php7.0-fpm/pool.conf >> /etc/php/7.0/fpm/pool.d/www.conf
 rm /container/service/roundcube/assets/php7.0-fpm/pool.conf
 
+cp -f /container/service/roundcube/assets/php7.0-fpm/opcache.ini /etc/php/7.0/fpm/conf.d/opcache.ini
+rm /container/service/roundcube/assets/php7.0-fpm/opcache.ini
+
 mkdir -p /var/www/tmp
 chown www-data:www-data /var/www/tmp
 
 # remove apache default host
 a2dissite 000-default
 rm -rf /var/www/html
+
+# Add apache modules
+a2enmod deflate expires
